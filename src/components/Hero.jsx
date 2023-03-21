dd13810f3b26617e3824e0e45c1f739a4a1dcc25
mirdzokhid1 <mirzohidmizamov.z@gmail.com>
2023-03-21 11:05:55 +0300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="main">
       <div className="main_text">
-        <span className="main_text-info">Chase the new Flavour</span>
+        <span className="main_text-info">Lorem ipsum dolor sit.</span>
         <h1 className="main_text-title">The key to Fine dining</h1>
         <p className="main_text-text">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit natus quo consequuntur nostrum pariatur eum enim! Repudiandae fugiat totam molestias!      
