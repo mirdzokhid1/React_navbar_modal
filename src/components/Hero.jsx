57bd61d669d817909b8ab259ef6e43f4c504ed4c
mirdzokhid1 <mirzohidmizamov.z@gmail.com>
2023-03-21 10:06:57 +0300

    chnge

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import heroImage from './hero_img.png';
+
+import './hero.scss';
+
+
+const Hero = () => {
+  return (
+    <div className="main">
+      <div className="main_text">
+        <span className="main_text-info">Chase the new Flavour</span>
+        <h1 className="main_text-title">The key to Fine dining</h1>
+        <p className="main_text-text">
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+          volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+          aliquam amet tellus{" "}
+        </p>
+        <button className="main_text-btn">Explore Menu</button>
+      </div>
+
+      <div className="main_img">
+        <img src={heroImage} alt="Kebab" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero
